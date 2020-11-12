3e26576861bbfe8131700fab5828259f7e3d8d59
livoras <me@livoras.com>
2020-11-12 09:44:30 +0800

    miao

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  public bottomText: string = 'web';
+export class AppComponent implements OnInit {
+  public bottomText = 'web';
+  public canvas: HTMLCanvasElement = null;
+  public ctx: CanvasRenderingContext2D;
+  public bgColor = '#ccc';
+  public imagePath = 'assets/logo.jpeg';
+  public width = 1080;
+  public height = 1920;
+  @ViewChild('image', { read: ElementRef, static: true })
+  public imageRef: ElementRef;
+
+  ngOnInit(): void {
+    this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    this.ctx = this.canvas.getContext('2d');
+    this.initRendering();
+  }
+
+  public initRendering(): void {
+    requestAnimationFrame(() => {
+      this.ctx.save();
+      this.render();
+      this.ctx.restore();
+      this.initRendering();
+    });
+  }
+
+  public render(): void {
+    const ctx = this.ctx;
+    ctx.fillStyle = this.bgColor;
+    ctx.fillRect(0, 0, 10000, 10000);
+
+    const imageElement = this.imageRef.nativeElement;
+    ctx.drawImage(
+      imageElement,
+      0,
+      (this.height - this.width) / 2,
+      this.width,
+      this.width,
+      );
+  }
 }
